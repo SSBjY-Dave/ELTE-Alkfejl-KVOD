7fc3a9f3cd98c8bebd48ea6eed833e6dcd0b12f7
David Orcsik <david.orcsik.prog@gmail.com>
2020-02-15 22:43:05 +0100

    Added spring backed base

---
+++ src/main/java/hu/elte/alkfejl/DormRooms/DormRoomsApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.alkfejl.DormRooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormRoomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormRoomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/elte/alkfejl/DormRooms/DormRoomsApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.elte.alkfejl.DormRooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormRoomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
